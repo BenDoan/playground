1a78a3f61bbc5740e67b8390ceb3745f96c94290
Ben Doan <ben@bendoan.me>
2015-03-30 23:11:58 -0500

    added time tracking and started getting settings

--- golang/src/github.com/bendoan/govis/govis.go
+++ golang/src/github.com/bendoan/govis/govis.go
@@ -1,22 +1,68 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/op/go-logging"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"regexp"
+	"time"
 )
 
 var log = logging.MustGetLogger("Govis")
 var format = logging.MustStringFormatter("%{color}[%{id:03x}] %{time:15:04:05} %{level:.4s}%{color:reset} %{message}")
+
+var configFileName = "govis.json"
 
 func main() {
 	backend := logging.NewLogBackend(os.Stderr, "", 0)
 	backendFormatter := logging.NewBackendFormatter(backend, format)
 	logging.SetBackend(backendFormatter)
 
-	fmt.Println(GetCurrentWindowName())
+	c := JsonCfg{}
+	fmt.Println(c.GetConfigFile(configFileName))
+
+	TrackTime()
+}
+
+type JsonCfg struct {
+	TickInterval int
+	LogName      string
+}
+
+func (I *JsonCfg) GetConfigFile(path string) *JsonCfg {
+	b, err := ioutil.ReadFile(path)
+
+	if err != nil {
+		log.Error("%s", err)
+	}
+
+	err = json.Unmarshal(b, &I)
+
+	if err != nil {
+		log.Error("%s", err)
+	}
+	return I
+}
+
+func TrackTime() {
+	lastTime := time.Now()
+	lastWindow := GetCurrentWindowName()
+
+	c := time.Tick(400 * time.Millisecond)
+	for now := range c {
+		currentWindow := GetCurrentWindowName()
+
+		if currentWindow != lastWindow {
+			timeDiff := time.Now().Sub(lastTime)
+			fmt.Printf("Changed from %s to %s for %s", lastWindow, currentWindow, timeDiff)
+		}
+
+		lastWindow = currentWindow
+		var _ = now
+	}
 }
 
 func GetCurrentWindowID() string {
@@ -45,8 +91,6 @@ func GetCurrentWindowName() string {
 		log.Warning("Could get current window name: %s", err)
 		return ""
 	}
-
-	fmt.Println(string(out))
 
 	re := regexp.MustCompile(`"(.*)"`)
 	match := re.FindStringSubmatch(string(out))
