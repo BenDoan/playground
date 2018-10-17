d674b408c5fc29828111223c248ae31112a60c7b
Ben Doan <ben@bendoan.me>
2018-10-17 09:00:24 -0500

    integrated symbol table with compiler

--- rust/cminus-compiler/src/compiler.rs
+++ rust/cminus-compiler/src/compiler.rs
@@ -2,9 +2,15 @@ use ast::{Meta, Program, Parameter, Stmt, Expr, Operator};
 use std::collections::HashMap;
 use util::get_pos;
 
+type SymbolTables = Vec<HashMap<String, SymbolEntry>>;
+
+pub struct SymbolEntry {
+    param: Parameter,
+}
+
 pub fn compile(program: &Program, source_code: &String) {
-    fn traverse(stmt: &Meta<Stmt>, source_code: &String) {
-        println!("{:?}", (
+    fn traverse(stmt: &Meta<Stmt>, symbol_tables: &mut SymbolTables, source_code: &String) {
+        println!("\n{:?}", (
             "comment",
             format!(
                 "line {}",
@@ -14,78 +20,9 @@ pub fn compile(program: &Program, source_code: &String) {
 
         match stmt.inside {
             Stmt::Block(ref stmts) => {
-                for stmt in stmts {
-                    traverse(&stmt, source_code);
-                }
-            }
-            Stmt::Function(_, _, ref stmt) => {
-                traverse(&*stmt, source_code);
-            }
-            Stmt::While(_, ref statement) => traverse(&*statement, source_code),
-            Stmt::For(_, _, _, ref statement) => traverse(&*statement, source_code),
-            Stmt::Expr(ref expr) => {
-                compile_expr(&expr);
-            }
-            _ => (),
-        }
-    }
-
-    for statement in program {
-        traverse(statement, source_code);
-    }
-}
-
-pub fn compile_expr(expr: &Meta<Expr>) -> usize {
-    match expr.inside {
-        Expr::Binary(ref op, ref e1, ref e2) => {
-            let e1_val = compile_expr(&*e1);
-            let e2_val = compile_expr(&*e2);
-            println!("{:?}", (
-                "binary-math-op",
-                op,
-                e1_val,
-                e2_val,
-                expr.byte_offset,
-            ));
-            expr.byte_offset
-
-        }
-        Expr::Unary(ref op, ref e1) => {
-            let e1_val = compile_expr(&*e1);
-            println!("{:?}", ("unary-math-op", op, e1_val));
-            expr.byte_offset
-
-        }
-        Expr::Assignment(ref lhs, ref rhs) => {
-            let lhs_val = compile_expr(&*lhs);
-            let rhs_val = compile_expr(&*rhs);
-            rhs_val
-        }
-        Expr::Identifier(ref name) => {
-            println!("{:?}", ("load", expr.byte_offset, name));
-            expr.byte_offset
-        }
-        Expr::Number(num) => {
-            println!("{:?}", ("word", expr.byte_offset, num));
-            expr.byte_offset
-        }
-        _ => 0,
-    }
-}
-
-struct SymbolEntry {
-    param: Parameter,
-}
-
-type SymbolTables = Vec<HashMap<String, SymbolEntry>>;
-
-pub fn process_symbol_table(program: &Program) {
-    fn traverse_stmt(stmt: Meta<Stmt>, symbol_tables: &mut SymbolTables) {
-        match stmt.inside {
-            Stmt::Block(stmts) => {
                 symbol_tables.push(HashMap::new());
                 for stmt in stmts {
-                    traverse_stmt(stmt, symbol_tables);
+                    traverse(&stmt, symbol_tables, source_code);
                 }
 
                 if let Some(table_for_scope) = symbol_tables.pop() {
@@ -113,7 +50,7 @@ pub fn process_symbol_table(program: &Program) {
                     }
                 }
             }
-            Stmt::Function(_, parameters, statement) => {
+            Stmt::Function(ref _name, ref parameters, ref statement) => {
                 for parameter in parameters {
                     println!(
                         "new param {:?} in scope at line {}",
@@ -127,16 +64,86 @@ pub fn process_symbol_table(program: &Program) {
                         );
                     }
                 }
-                traverse_stmt(*statement, symbol_tables);
+                traverse(&*statement, symbol_tables, source_code);
             }
-            Stmt::While(_, statement) => traverse_stmt(*statement, symbol_tables),
-            Stmt::For(_, _, _, statement) => traverse_stmt(*statement, symbol_tables),
-            _ => (),
+            Stmt::While(_, ref statement) => traverse(&*statement, symbol_tables, source_code),
+            Stmt::For(_, _, _, ref statement) => traverse(&*statement, symbol_tables, source_code),
+            Stmt::Expr(ref expr) => {
+                compile_expr(&expr, symbol_tables);
+            }
+            _ => {
+                println!("{:?}", ("unimplemented",));
+            }
         }
     }
 
     let mut symbol_tables: SymbolTables = vec![];
     for statement in program {
-        traverse_stmt(statement.clone(), &mut symbol_tables);
+        traverse(statement, &mut symbol_tables, source_code);
+    }
+}
+
+pub fn compile_expr(expr: &Meta<Expr>, symbol_tables: &mut SymbolTables) -> usize {
+    match expr.inside {
+        Expr::Binary(ref op, ref e1, ref e2) => {
+            match op {
+                Operator::LogicalAnd => {
+                    let e1_loc = compile_expr(&*e1, symbol_tables);
+                    println!(
+                        "{:?}",
+                        ("test", e1_loc, "if false jump to", expr.byte_offset)
+                    );
+                    let e2_loc = compile_expr(&*e2, symbol_tables);
+                    println!("{:?}", ("label", expr.byte_offset));
+                    println!("{:?}", ("and", e1_loc, e2_loc, expr.byte_offset));
+                }
+                Operator::LogicalOr => {
+                    let e1_loc = compile_expr(&*e1, symbol_tables);
+                    println!(
+                        "{:?}",
+                        ("test", e1_loc, "if true jump to", expr.byte_offset)
+                    );
+                    let e2_loc = compile_expr(&*e2, symbol_tables);
+                    println!("{:?}", ("label", expr.byte_offset));
+                    println!("{:?}", ("and", e1_loc, e2_loc, expr.byte_offset));
+                }
+                _ => {
+                    let e1_val = compile_expr(&*e1, symbol_tables);
+                    let e2_val = compile_expr(&*e2, symbol_tables);
+                    println!("{:?}", (
+                        "binary-math-op",
+                        op,
+                        e1_val,
+                        e2_val,
+                        expr.byte_offset,
+                    ));
+                }
+            };
+            expr.byte_offset
+        }
+        Expr::Unary(ref op, ref e1) => {
+            let e1_val = compile_expr(&*e1, symbol_tables);
+            println!("{:?}", ("unary-math-op", op, e1_val, expr.byte_offset));
+            expr.byte_offset
+
+        }
+        Expr::Assignment(ref lhs, ref rhs) => {
+            let _var_name = match lhs.inside {
+                Expr::Identifier(ref s) => s.to_string(),
+                _ => "".to_string(),
+            };
+            let rhs_val = compile_expr(&*rhs, symbol_tables);
+            println!("{:?}", ("store", rhs_val, lhs.byte_offset));
+            lhs.byte_offset
+        }
+        Expr::Identifier(ref name) => {
+            println!("{:?}", ("load", expr.byte_offset, name));
+            expr.byte_offset
+        }
+        Expr::Number(num) => {
+            println!("{:?}", ("word", expr.byte_offset, num));
+            expr.byte_offset
+        }
+        _ => 0,
     }
 }
